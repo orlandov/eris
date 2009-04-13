2de745d25935ab7719dc76094e74b653d2ece041
Orlando Vazquez <orlandovazquez@2wycked.local>
2009-04-13 15:22:32 -0700

    add some scanner tests and test functions

--- eris.py
+++ eris.py
@@ -2,29 +2,61 @@
 
 from Plex import *
 
-def ErisScanner(Scanner):
-    pass
+from StringIO import StringIO
 
-def create_lexicon():
-    letter = Range("AZaz")
+class ErisScanner(Scanner):
+    def open_bracket_action(self, text):
+        self.bracket_nesting_level = self.bracket_nesting_level + 1
+        return text
+
+    def close_bracket_action(self, text):
+        self.bracket_nesting_level = self.bracket_nesting_level - 1
+        return text
+
+    letter = Range("AZaz") | Any("_")
     digit = Range("09")
-    string = Str('"') + Rep(AnyBut('"')) + Str('"')
-    name = letter + Rep(letter | digit)
-    number = Rep1(digit)
-    float = Rep1(number) + Str('.') + Rep1(number)
-    space = Any(" \t\n")
+    hexdigit = Range("09AFaf")
+
+    identifier = letter + Rep(letter | digit)
+    number = Rep1(digit) | (Str("0x") + Rep1(hexdigit)) | (Rep1(digit) + Str('.') + Rep1(digit))
+    
+    sq_string = (
+        Str("'") + 
+        Rep(AnyBut("\\\n'") | (Str("\\") + AnyChar)) + 
+        Str("'"))
+        
+    dq_string = (
+        Str('"') + 
+        Rep(AnyBut('\\\n"') | (Str("\\") + AnyChar)) + 
+        Str('"'))
+            
+    stringlit = sq_string | dq_string
+    opening_bracket = Any("([{")
+    closing_bracket = Any(")]}")
+    punct1 = Any(":,;+-*/|&<>=.%`~^")
+    punct2 = Str("==", "<>", "!=", "<=", "<<", ">>", "**")
+    punctuation = punct1 | punct2
+
+    spaces = Rep1(Any(" \t"))
+    empty_line = Rep(spaces) + Str("\n")
+    indentation = Rep(Str(" ")) | Rep(Str("\t"))
+    escaped_newline = Str("\\\n")
     comment = Str("#") + Rep(AnyBut("\n"))
-
-    resword = Str("if", "then", "else", "end")
-
-    lex = Lexicon([
-        (comment,         'comment'),
-        (name,            'ident'),
-        (number,          'int'),
-        (float,           'float'),
-        (resword,         TEXT),
-        (Any("+-*/=<>"),  TEXT),
-        (space | comment, IGNORE)
+    blank_line = indentation + Opt(comment)
+    
+    lexicon = Lexicon([
+        (identifier,      'identifier'),
+        (number,          'number'),
+        (stringlit,       'string'),
+        (punctuation,     TEXT),
+        (opening_bracket, open_bracket_action),
+        (closing_bracket, close_bracket_action),
+        (comment,         IGNORE),
+        (spaces,          IGNORE),
+        (empty_line,      IGNORE),
+        (escaped_newline, IGNORE),
     ])
 
-    return lex
+    def __init__(self, source):
+        Scanner.__init__(self, self.lexicon, StringIO(source))
+        self.bracket_nesting_level = 0
--- tests/test_eris.py
+++ tests/test_eris.py
@@ -2,44 +2,111 @@
 
 import unittest
 from cStringIO import StringIO
-
-
-src = """
-"string"
-420
-6.66
-"""
-
 import sys
-sys.path.append('plex')
 
+sys.path.append('plex')
 from Plex import *
+
 import eris
 
-class LexTest(unittest.TestCase):
-    def setUp(self):
-        self.lexicon = eris.create_lexicon()
-
+class LexTest(object):
     def mk_scanner(self, source):
-        f = StringIO(source)
-        return Scanner(self.lexicon, f, 'test')
+        return eris.ErisScanner(source)
 
-class TestNumber(LexTest):
+    def assertScan(self, src, exp_tokens):
+        s = self.mk_scanner(src)
+        self.assertEqual(list(s.tokens()), exp_tokens)
+
+
+class TestNumber(unittest.TestCase, LexTest):
     def test_digit(self):
         src = "4"
-        s = self.mk_scanner(src)
-        self.assertEqual(list(s.tokens()), [('int', '4')])
+        self.assertScan(src, [('number', '4')])
 
     def test_number(self):
         src = "44"
-        s = self.mk_scanner(src)
-        self.assertEqual(list(s.tokens()), [('int', '44')])
+        self.assertScan(src, [('number', '44')])
 
     def test_float(self):
         src = "42.666"
-        s = self.mk_scanner(src)
-        self.assertEqual(list(s.tokens()), [('float', '42.666')])
+        self.assertScan(src, [('number', '42.666')])
 
-class TestComment(LexTest):
+
+class TestEmpty(unittest.TestCase, LexTest):
+    def assertEmpty(self, l):
+        self.assertEqual(l, [])
+
+    def test_empty(self):
+        src = ""
+        s = self.mk_scanner(src)
+        self.assertEmpty(list(s.tokens()))
+
+    def test_blanklines(self):
+        src = "\n\n\n"
+        s = self.mk_scanner(src)
+        self.assertEmpty(list(s.tokens()))
+
+
+class TestComment(unittest.TestCase, LexTest):
     def test_comment(self):
+        src = "# foo\n4"
+        self.assertScan(src, [('number', '4')])
+
+    def test_comment(self):
+        src = "42 # comment"
+        self.assertScan(src, [('number', '42')])
+
+class TestString(unittest.TestCase, LexTest):
+    def test_string(self):
+        src = '"master shake"'
+        self.assertScan(src, [('string', '"master shake"')])
+
+
+class TestMultiple(unittest.TestCase, LexTest):
+    def test_multiple(self):
+        src = "42\n'call me'"
+        self.assertScan(src, [
+            ('number', "42"),
+            ('string', "'call me'")
+        ])
+
+    def test_name(self):
         src = "foo"
+        self.assertScan(src, [('identifier', 'foo')])
+
+class TestPunctuation(unittest.TestCase, LexTest):
+    def test_punct(self):
+        src = "42 + 420"
+        self.assertScan(src, [
+            ('number', '42'),
+            ('+', '+'),
+            ('number', '420')
+        ])
+
+
+class TestFunction(unittest.TestCase, LexTest):
+    def test_function(self):
+        src = """
+            a = function (foo, bar, baz) {
+                statement1
+                statement2
+            }
+            b = a;
+        """
+
+        self.assertScan(src, [
+            ('identifier', 'a'),
+            ('=', '='),
+            ('identifier', 'function'),
+            ('(', '('),
+            ('identifier', 'foo'),
+            (',', ','),
+            ('identifier', 'bar'),
+            (',', ','),
+            ('identifier', 'baz'),
+            (')', ')'),
+            ('{', '{'),
+            ('identifier', 'statement1',),
+            ('identifier', 'statement2',),
+            ('}', '}'),
+        ])
